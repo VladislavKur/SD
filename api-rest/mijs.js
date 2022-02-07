85fad8449b3b528e8326364d610bddb06bb5b158
Vladislavkur <vladis26.1.97@gmail.com>
2022-02-07 20:47:50 +0100

    servidor

--- api-rest/mijs.js
+++ api-rest/mijs.js
@@ -1,4 +1,9 @@
-
-function popUp(){
-    alert("Hola alerta");
-}
+ 
+var http = require('http'); 
+ 
+http.createServer( (request, response) => { 
+    response.writeHead(200, {'Content-Type': 'text/plain'}); 
+    response.end('Hola a todas y a todos!\n'); 
+}).listen(8080); 
+ 
+console.log('Servidor ejecutándose en puerto 8080...');
