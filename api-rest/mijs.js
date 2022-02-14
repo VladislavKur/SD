5868f7c254ed9cc56cdd7d8488fbcf277be4c42f
Vladislavkur <vladis26.1.97@gmail.com>
2022-02-14 19:23:03 +0100

    nodemon port

--- api-rest/mijs.js
+++ api-rest/mijs.js
@@ -1,22 +1,8 @@
- 
-/*var http = require('http'); 
-var server = http.createServer(); 
- 
-function HTTP_Response(request, response) { 
-    response.writeHead(200, {'Content-Type': 'text/plain'}); 
-    response.write('Hola a todas y a todos!\n'); 
-    response.end(); 
-} 
- 
-server.on('request', HTTP_Response); 
-server.listen(8080); 
- 
-console.log('Servidor ejecutándossdsadadasd en puerto 8080...');
-*/
+
 
 'use strict'
 
-const port = process.env.port || 8888;
+const port = process.env.port || 3000;
 const express = require('express');
 
 const app = express();
