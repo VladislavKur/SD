660e4cd006d89c8ae139479637a439fb9e370fc0
Vladislavkur <vladis26.1.97@gmail.com>
2022-02-14 19:58:13 +0100

    añadido lector de body para POST

--- api-rest/mijs.js
+++ api-rest/mijs.js
@@ -3,14 +3,39 @@
 'use strict'
 
 const port = process.env.port || 3000;
+const { request, response } = require('express');
 const express = require('express');
+const logger = require('morgan');
 
-const app = express();
+const app = express();  
+app.use(logger('dev'));
 
-app.get('/hola/:Estudiantes', (request, response) => {
+// Declaramos los middleware 
+app.use(express.urlencoded({ extended: false })); 
+app.use(express.json());
+
+/*app.get('/hola/:Estudiantes', (request, response) => {
     response.status(200).send({ mensaje: `Hola ${request.params.Estudiantes} desde SD`})
 
+});*/
+
+app.get( '/api/products', (request, response) =>{
+    response.status(200).send({products :[]});
+});
+app.get( '/api/products/:productID', (request, response) =>{
+    response.status(200).send({products :`${request.params.productID}`});
+});
+app.post( '/api/products', (request, response) =>{
+    console.log(request.body);
+    response.status(200).send({products :'El producto se ha recibido'});
+});
+app.put('/api/products/:productID', (request, response) =>{
+    response.status(200).send({products :`${request.params.productID}`});
+});
+app.delete('/api/products/:productID', (request,response) =>{
+    request.status(200).send(({products :`${request.params.productID}`}));
 });
 app.listen(port , () => {
     console.log(` API ejecutandose desde hhtp//localhost:${port}/hola/Estudiantes`);
 });
+
