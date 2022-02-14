402ed84620040b7756603dcdd8f1dadb18a6ab02
Vladislavkur <vladis26.1.97@gmail.com>
2022-02-14 18:09:59 +0100

    express

--- api-rest/mijs.js
+++ api-rest/mijs.js
@@ -1,5 +1,5 @@
  
-var http = require('http'); 
+/*var http = require('http'); 
 var server = http.createServer(); 
  
 function HTTP_Response(request, response) { 
@@ -12,3 +12,17 @@ server.on('request', HTTP_Response);
 server.listen(8080); 
  
 console.log('Servidor ejecutándossdsadadasd en puerto 8080...');
+*/
+
+'use strict'
+
+const { response } = require('express');
+const express = require('express');
+const app = express();
+app.get('/hola', (request, response) => {
+    response.send('Holita... esto funciona');
+
+});
+app.listen(8080, () => {
+    console.log("console log");
+})
