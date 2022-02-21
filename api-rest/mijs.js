d00af51f03e9abde8253de882a1ea3047f4c2fd3
Vladislavkur <vladis26.1.97@gmail.com>
2022-02-21 21:19:12 +0100

    sin base de datos pero todos los servicios

--- api-rest/mijs.js
+++ api-rest/mijs.js
@@ -13,26 +13,47 @@ const app = express();
 
 // Declaramos los middleware 
 app.use(logger('dev'));
-app.use(express.urlencoded({ extended: false })); 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get( '/api/product', (request, response) =>{
-    response.status(200).send({products :[]});
-});
+// el servicio se puede llamar a una funcion o crearlo directamente
+app.get( '/api/product', getProductController); 
+
+
 app.get( '/api/product/:productID', (request, response) =>{
     response.status(200).send({products :`${request.params.productID}`});
 });
 app.post( '/api/product', (request, response) =>{
     console.log(request.body);
-    response.status(200).send({products :'El producto se ha recibido'});
+    response.status(200).send({product : request.body });
 });
+//pasamos el ID por valor 
 app.put('/api/product/:productID', (request, response) =>{
-    response.status(200).send({products :`${request.params.productID}`});
+    const id = request.params.productID;
+    const nuevosDatos = request.body;
+    response.status(200).send({
+        msg: "Actualizando Nuevos datos",
+        id: id,
+        "Nuevo: " :nuevosDatos
+    });
 });
+//borramos por id, 
 app.delete('/api/product/:productID', (request,response) =>{
-    request.status(200).send(({products :`${request.params.productID}`}));
+    const id = request.params.productID;
+    
+    request.status(200).send(({
+        msg: "Se ha eliminado el prducto",
+        product :id
+    }));
 });
 app.listen(port , () => {
-    console.log(` API ejecutandose desde http//localhost:${port}/hola/Estudiantes`);
+    console.log(` API RESTFul CRUD ejecutandose desde http//localhost:${port}/api/product`);
 });
 
+
+function getProductController(request, response){
+    response.status(200).send({
+        msg: "Todos los productos",
+        product : []
+    });
+};
