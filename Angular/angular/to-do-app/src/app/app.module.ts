650dc5ffc89005099e9faeaf48435f03c279d588
Vladislavkur <vladis26.1.97@gmail.com>
2022-04-12 17:25:30 +0200

    guia 9

--- Angular/angular/to-do-app/src/app/app.module.ts
+++ Angular/angular/to-do-app/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
 
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module'; // IMPORTANTE EN LA GUIA NO SALE EL ROUTING
+
+import { TaskService } from './services/task.service';  
 import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component'
+
 
 @NgModule({
   declarations: [
@@ -13,9 +19,12 @@ import { TasksComponent } from './components/tasks/tasks.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
