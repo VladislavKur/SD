f67cf709e1219e9b1575a7e07ebb5df8ed28ff3e
Vladislavkur <vladis26.1.97@gmail.com>
2022-05-02 19:03:05 +0200

    Practica 10

--- Angular/angular/to-do-app/src/app/app.component.ts
+++ Angular/angular/to-do-app/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'TITULO DE LA PAGINA';
+import { Component, OnInit } from '@angular/core'; 
+import { UserService } from "./services/user.service"; 
+ 
+@Component({ 
+  selector: 'app-root', 
+  templateUrl: './app.component.html', 
+  styleUrls: ['./app.component.css'] 
+}) 
+export class AppComponent implements OnInit { 
+  title = 'toDoApp para SD'; 
+ 
+  email:    string = 'test'; 
+  password: string = 'test'; 
+   
+  constructor(public userService: UserService) {} 
+ 
+  ngOnInit() {  
+    this.login(); 
+  } 
+ 
+  login() { 
+    const user = { email: this.email, pass: this.password }; 
+    this.userService.signIn(user).subscribe( 
+      (loggedUser) => { 
+        this.userService.setToken(loggedUser.token); 
+        console.log("Login correcto"); 
+        console.log(loggedUser.token); 
+      }, 
+      (err) => {
+        console.error("Error en el login"); 
+        console.log(err); 
+      } 
+    ); 
+  } 
 }
